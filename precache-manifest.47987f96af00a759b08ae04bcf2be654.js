b92fe95a8d8036c064c6954013eb19964bc8166d
Luiz Chagas <luiz@crema.us>
2019-02-22 14:38:54 -0600

    Updates

---
+++ precache-manifest.47987f96af00a759b08ae04bcf2be654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d158d20be91882c6726",
+    "url": "/plant-web/static/js/runtime~main.3d158d20.js"
+  },
+  {
+    "revision": "30d18a3c3a1de1cd229d",
+    "url": "/plant-web/static/js/main.30d18a3c.chunk.js"
+  },
+  {
+    "revision": "4066774a85fd08d137c6",
+    "url": "/plant-web/static/js/2.4066774a.chunk.js"
+  },
+  {
+    "revision": "30d18a3c3a1de1cd229d",
+    "url": "/plant-web/static/css/main.351477f1.chunk.css"
+  },
+  {
+    "revision": "27c1e9a07ebd64cd17103740fbb11525",
+    "url": "/plant-web/index.html"
+  }
+];
